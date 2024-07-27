1d237bed603d280e1d78253646601cdb736cdf0e
Anjudimpy <anjukr55555@gmail.com>
2024-07-27 16:27:31 +0530

    newt

--- app/Componets/MobileNav.tsx
+++ app/Componets/MobileNav.tsx
@@ -1,4 +1,5 @@
 import { XMarkIcon } from '@heroicons/react/20/solid'; // Ensure correct import path
+import Link from 'next/link';
 import React from 'react';
 
 interface Props {
@@ -11,12 +12,12 @@ const MobileNav = ({ nav, closeNav }: Props) => {
   return (
     <div className={`fixed ${navAnimation} transform transition-all duration-300 top-0 left-0 right-0 bottom-0 z-[1000000] bg-[#09101a]`}>
       <div className='w-[100vw] h-[100vh] flex flex-col items-center justify-center'>
-        <div className="nav-link-mobile">HOME</div>
-        <div className="nav-link-mobile">SERVICES</div>
-        <div className="nav-link-mobile">ABOUT</div> 
-        <div className="nav-link-mobile">PROJECTS</div>
-        <div className="nav-link-mobile">BLOG</div>
-        <div className="nav-link-mobile">CONTACT</div>
+      <Link href="#"><div className="nav-link-mobile">HOME</div></Link>
+<Link href="#services"><div className="nav-link-mobile">SERVICES</div></Link>
+<Link href="#about"><div className="nav-link-mobile">ABOUT</div> </Link>
+<Link href="#projects"><div className="nav-link-mobile">PROJECTS</div></Link>
+{/* <Link href="#"><div className="nav-link">BLOG</div></Link> */}
+<Link href="#footer"><div className="nav-link-mobile">CONTACT</div></Link>
       </div>
       <div onClick={closeNav} className='absolute z-[100000000] cursor-pointer top-[2rem] right-[2rem] w-[2rem] h-[2rem] text-yellow-400'>
         <XMarkIcon />
