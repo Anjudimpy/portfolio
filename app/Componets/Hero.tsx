08826adc3840128fe6af9f7a46557942536acff4
Anjudimpy <anjukr55555@gmail.com>
2024-07-27 13:07:21 +0530

    fit

--- app/Componets/Hero.tsx
+++ app/Componets/Hero.tsx
@@ -12,7 +12,7 @@ const Hero = () => {
         <div className='w-[80%] grid-cols-1 mx-auto grid lg:grid-cols-2 gap-[3rem] h-[100%] items-center'>
 <div>
 <h1 className='text-[35px] md:text-[50px] text-white font-bold'>
-  HI, I'M <span className='text-yellow-300'>ANJU</span>
+  HI{','} I AM <span className='text-yellow-300'>ANJU</span>
 </h1>
 
 <TextEffect/>
