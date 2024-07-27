3319145bd696e087d4e84abf33de43edc076e890
Anjudimpy <anjukr55555@gmail.com>
2024-07-27 12:45:01 +0530

    first

--- app/Componets/Footer.tsx
+++ app/Componets/Footer.tsx
@@ -13,8 +13,8 @@ const Footer = () => {
         </h1>
     <div className='grid border-b-[1px] pb-[6rem] border-gray-400 grid-cols-1 lg:grid-cols-3 md:grid-cols-2
     w-[80%] mx-auto gap-[3rem]'>
-    <div className=' relative text-red-600  animate-spin bg-yellow-300  -left-1  w-[6.5rem] h-[6.5rem] border border-t-2  rounded-full'>
-        <div className='text-red-600   -left-1  w-[6.5rem] h-[6.5rem] border border-t-2  rounded-full'></div>
+    <div className=' relative text-red-600  animate-spin bg-yellow-300  -left-1  md:w-[6.5rem] md:h-[6.5rem] w-[5.5rem] h-[5.5rem] border border-t-2  rounded-full'>
+        <div className='text-red-600   -left-1  md:w-[6.5rem] md:h-[6.5rem] w-[5.5rem] h-[5.5rem] border border-t-2  rounded-full'></div>
     </div>
 <div className=' group flex items-center  absolute space-x-6'>
     <div className='md:w-[6rem] md:h-[6rem]  w-[5rem] h-[5rem] flex items-center justify-center rounded-full  bg-[#55e6a5]'>
@@ -30,8 +30,12 @@ const Footer = () => {
 </div>
 
 
-<div className=' group flex items-center space-x-6'>
-    <div className='md:w-[6.5rem] md:h-[6.5rem] w-[5rem] h-[5rem] flex items-center justify-center rounded-full bg-[#55e6a5]'>
+<div>
+<div className=' relative text-red-600  animate-spin bg-yellow-300  -left-1 w-[5.5rem] h-[5.5rem] md:w-[6.5rem] md:h-[6.5rem] border border-t-2  rounded-full'>
+        <div className='text-red-600   -left-1  w-[5.5rem] h-[5.5rem] md:w-[6.5rem] md:h-[6.5rem] border border-t-2  rounded-full'></div>
+    </div>
+<div className=' group absolute md:-mt-[6.3rem] -mt-[5.5rem] md:ml-1 ml-0.5 flex items-center space-x-6'>
+    <div className='md:w-[6rem] md:h-[6rem] w-[5rem] h-[5rem] flex items-center justify-center rounded-full bg-[#55e6a5]'>
 <DevicePhoneMobileIcon className='md:w-[5rem] md:h-[5rem] w-[3.5rem] h-[3.5rem] text-white group-hover:animate-spin'/>
     </div>
     <div>
@@ -41,10 +45,15 @@ const Footer = () => {
         <p className='text-[17px] w-[90%] text-white opacity-60 group-hover:text-[#55e6a5]'>+918294403079 <br/>+917061336192</p>
     </div>
 </div>
+</div>
 
 
-<div className='group flex items-center space-x-6'>
-    <div className='md:w-[6.5rem] md:h-[6.5rem] w-[5rem] h-[5rem] flex items-center justify-center rounded-full bg-[#55e6a5]'>
+<div>
+<div className=' relative text-red-600  animate-spin bg-yellow-300  -left-1 w-[5.5rem] h-[5.5rem] md:w-[6.5rem] md:h-[6.5rem] border border-t-2  rounded-full'>
+        <div className='text-red-600   -left-1  w-[5.5rem] h-[5.5rem] md:w-[6.5rem] md:h-[6.5rem] border border-t-2  rounded-full'></div>
+    </div>
+<div className='group absolute md:-mt-[6.3rem] -mt-[5.3rem] flex items-center space-x-6'>
+    <div className='md:w-[6rem] md:h-[6rem] w-[5rem] h-[5rem] flex items-center justify-center rounded-full bg-[#55e6a5]'>
 <EnvelopeIcon className='md:w-[5rem] md:h-[5rem] w-[3.5rem] h-[3.5rem] text-white group-hover:animate-pulse'/>
     </div>
     <div>
@@ -54,6 +63,7 @@ const Footer = () => {
         <p className='text-[17px] w-[90%] text-white opacity-60 group-hover:text-[#55e6a5]'>anjukr@55555gmail.com</p>
     </div>
 </div>
+</div>
 
 
 <div>
@@ -61,7 +71,7 @@ const Footer = () => {
         <div className='text-red-600   -left-1  w-[5.5rem] h-[5.5rem] md:w-[6.5rem] md:h-[6.5rem] border border-t-2  rounded-full'></div>
     </div>
 
-<div className='group absolute flex md:-mt-[6.3rem] -mt-[5.4rem] -ml-1 items-center space-x-6'>
+<div className='group absolute flex md:-mt-[6.3rem] -mt-[5.4rem] md:-ml-1  md:-m-0 items-center space-x-6'>
     <div className='md:w-[6rem] md:h-[6rem] w-[5rem] h-[5rem] flex items-center justify-center rounded-full bg-[#55e6a5]'>
 <FaGithub className='md:w-[5rem] md:h-[5rem] w-[3.5rem] h-[3.5rem] group-hover:animate-ping text-white'/>
     </div>
@@ -76,10 +86,10 @@ const Footer = () => {
 </div>
 </div>
 <div>
-<div className=' relative text-red-600  animate-spin bg-yellow-300  -left-1  w-[6.5rem] h-[6.5rem] border border-t-2  rounded-full'>
-        <div className='text-red-600   -left-1  w-[6.5rem] h-[6.5rem] border border-t-2  rounded-full'></div>
+<div className=' relative text-red-600  animate-spin bg-yellow-300  -left-1  md:w-[6.5rem] md:h-[6.5rem] w-[5.5rem] h-[5.5rem] border border-t-2  rounded-full'>
+        <div className='text-red-600   -left-1  md:w-[6.5rem] md:h-[6.5rem] w-[5.5rem] h-[5.5rem] border border-t-2  rounded-full'></div>
     </div>
-<div className='group absolute -mt-[6rem] flex items-center space-x-6'>
+<div className='group absolute md:-mt-[6rem] -mt-[5rem] flex items-center space-x-6'>
     <div className='md:w-[6rem] md:h-[6rem] w-[5rem] h-[5rem] flex items-center justify-center rounded-full bg-[#55e6a5]'>
 <FaLinkedin className='md:w-[4.5rem] rounded-full md:h-[4.5rem] w-[3.5rem] h-[3.5rem] text-white group-hover:animate-ping'/>
     </div>
